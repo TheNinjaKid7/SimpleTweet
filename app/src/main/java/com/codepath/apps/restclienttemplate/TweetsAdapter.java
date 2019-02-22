83d50bdd997eb28f3526f8727ec3576b69f6490e
TheNinjaKid7 <gvaldez@bulldogs.aamu.edu>
2019-02-22 11:48:11 -0600

    SimpleTweet Twitter client app

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -49,6 +49,19 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         return tweets.size();
     }
 
+
+    // Clean all elements of the recycler
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items
+    public void addTweets(List<Tweet> tweetList) {
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
     // Define the Viewholder
     public class ViewHolder extends RecyclerView.ViewHolder{
 
